b45776473b2244b04117dd991b67aac3c1968c87
AABI Oumaima <aabioumaima@AABIs-MacBook-Pro.local>
2024-06-14 15:50:58 +0200

    new commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Maven!");
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
 
-<!--  This a new change after succuing the project but still have the maven problem, mv not found in jenkins  This is a comment in Maven POM file -->
